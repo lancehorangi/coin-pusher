deec853619f015a133462a25f663b1ba078fbe43
MaTHtoE <canyara@gmail.com>
2018-02-23 16:18:19 +0800

    Setup Pod

---
+++ ios/Pods/Target Support Files/Pods-coinpusher-tvOS/Pods-coinpusher-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_coinpusher_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_coinpusher_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-coinpusher-tvOSTests/Pods-coinpusher-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_coinpusher_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_coinpusher_tvOSTests
+@end
