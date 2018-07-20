a67292885a28df379809b3507147fce6f423467f
MaTHtoE <canyara@gmail.com>
2018-07-20 18:26:50 +0800

    修改测试服地址

--- js/env.js
+++ js/env.js
@@ -6,8 +6,10 @@ let _devMode = false;
 async function loadDevMode() {
   try {
     let devModeString = await AsyncStorage.getItem("DevMode");
-    _devMode = (devModeString === "true");
-    console.log("loadDevMode type _devMode:" + typeof _devMode + ", value:" + _devMode);
+    _devMode = devModeString === "true";
+    console.log(
+      "loadDevMode type _devMode:" + typeof _devMode + ", value:" + _devMode
+    );
   } catch (e) {
     console.error("loadDevMode err:" + e.message);
     _devMode = false;
@@ -38,19 +40,22 @@ let _env = {
   payServerUrl: "http://47.97.189.108:7010/1.0/",
   notifyUrl: "http://47.97.189.108:7010/1.0/pay/notify",
 
-  devServerURL: "http://47.97.189.108:7002/1.0/",
-  devPayServerUrl: "http://47.97.189.108:7011/1.0/",
-  devNotifyUrl: "http://47.97.189.108:7011/1.0/pay/notify",
+  devServerURL: "http://47.97.189.108:7100/1.0/",
+  devPayServerUrl: "http://47.97.189.108:7111/1.0/",
+  devNotifyUrl: "http://47.97.189.108:7111/1.0/pay/notify",
 
   talkingdataID: "B7E8A44ADCCE4C9ABBC2A9391E05E6A4",
   wxID: "wx03ea55f8013cb5e1",
   compatibleStoreVersion: "0.10",
-  updateToggleAddress: "https://circus.oss-cn-hangzhou.aliyuncs.com/toggle.json",
+  updateToggleAddress:
+    "https://circus.oss-cn-hangzhou.aliyuncs.com/toggle.json",
   PFBparaID: "60100111",
   PFBAppID: "12141",
   PFBKey: "10cf545073595d3e50d46eaa52b37bec",
-  codePushStageKey: "rPwFPpkVR_SrUzN6eAxA0IjGtxICbf415778-a89a-4469-9d3f-0bb68692a475",
-  codePushProductKey: "HSAwqzrXyJ1DifUxOao7b5PpjWEBbf415778-a89a-4469-9d3f-0bb68692a475"
+  codePushStageKey:
+    "rPwFPpkVR_SrUzN6eAxA0IjGtxICbf415778-a89a-4469-9d3f-0bb68692a475",
+  codePushProductKey:
+    "HSAwqzrXyJ1DifUxOao7b5PpjWEBbf415778-a89a-4469-9d3f-0bb68692a475"
 };
 
 function getCodePushKey() {
